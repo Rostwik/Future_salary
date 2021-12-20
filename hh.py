237f3dee92c72bca7ff190db8941fb11865bf468
Rostislav <rostwik@gmail.com>
2021-12-21 01:12:54 +0300

    Fixed the request settings

--- hh.py
+++ hh.py
@@ -4,6 +4,7 @@ import requests
 def predict_rub_salary(salary):
     if salary['currency'] != 'RUR':
         average_salary = None
+
     elif salary['from'] is not None and salary['to'] is None:
         average_salary = salary['from'] * 1.2
 
@@ -29,7 +30,6 @@ def get_hh_job_openings(programming_languages):
         while page < pages_number:
             payload = {
                 'text': language,
-                'only_with_salary': True,
                 'page': page,
                 'per_page': 20
             }
@@ -43,10 +43,10 @@ def get_hh_job_openings(programming_languages):
                 jobs.append(item)
 
         for job in jobs:
-
-            salary = predict_rub_salary(job['salary'])
-            if salary is not None:
-                salaries.append(salary)
+            if job['salary']:
+                salary = predict_rub_salary(job['salary'])
+                if salary is not None:
+                    salaries.append(salary)
 
         programming_languages[language]['vacancies_processed'] = len(salaries)
         try:
