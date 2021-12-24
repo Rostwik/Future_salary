bb4efe0be401cf0771e699e3d938f5fdcb52ce31
Rostislav <rostwik@gmail.com>
2021-12-25 01:12:52 +0300

    Refactoring

--- table_view.py
+++ table_view.py
@@ -2,7 +2,12 @@ from terminaltables import AsciiTable
 
 
 def get_vacancies_table(programming_languages, title):
-    column_headers = [['Язык программирования', 'Вакансий найдено', 'Вакансий обработано', 'Средняя зарплата']]
+    column_headers = [[
+        'Язык программирования',
+        'Вакансий найдено',
+        'Вакансий обработано',
+        'Средняя зарплата'
+    ]]
 
     for language, details in programming_languages.items():
         column_headers.append([
